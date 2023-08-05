394ecfbeaeb628ee19eb64a9fbbd288b9c13d8db
doikuomola <doikuomola@gmail.com>
2023-08-05 14:41:05 +0100

    deploy fixes

--- app/crud/page.tsx
+++ app/crud/page.tsx
@@ -2,7 +2,7 @@ import { AddPost, PostList } from '@/components';
 import React from 'react';
 
 async function getPostsData() {
-  const API_URI = process.env.API_URI
+  const API_URI = process.env.API_URI;
   const res = await fetch(`${API_URI}/api/posts`, {
     method: 'GET',
     cache: 'no-store',
@@ -19,7 +19,9 @@ export default async function Crud() {
   return (
     <main className="mt-1 md:mt-8">
       <div className="my-2 md:my-5 flex flex-col gap-4">
-        <h1 className="text-2xl md:text-3xl font-light md:font-bold">Todo List App</h1>
+        <h1 className="text-2xl md:text-3xl font-light md:font-bold">
+          Todo List App
+        </h1>
         <AddPost />
       </div>
 
