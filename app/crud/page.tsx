7575c97f9a121ab6f2f61f28c0ee2e284af1f384
doikuomola <doikuomola@gmail.com>
2023-08-06 06:10:21 +0100

    added postlist

--- app/crud/page.tsx
+++ app/crud/page.tsx
@@ -1,32 +1,31 @@
 import { AddPost, PostList } from '@/components';
 import React from 'react';
 
-// async function getPostsData() {
-//   const API_URI = process.env.API_URI;
-//   const res = await fetch(`${API_URI}/api/posts`, {
-//     method: 'GET',
-//     cache: 'no-store',
-//   });
+async function getPostsData() {
+  const API_URI = process.env.API_URI;
+  const res = await fetch(`${API_URI}/api/posts`, {
+    method: 'GET',
+    cache: 'no-store',
+  });
 
-//   const { posts } = await res.json();
+  const { posts } = await res.json();
 
-//   return posts;
-// }
+  return posts;
+}
 
 export default async function Crud() {
-  // const posts = await getPostsData();
+  const posts = await getPostsData();
 
   return (
     <main className="mt-1 md:mt-8">
-      {/* <div className="my-2 md:my-5 flex flex-col gap-4">
+      <div className="my-2 md:my-5 flex flex-col gap-4">
         <h1 className="text-2xl md:text-3xl font-light md:font-bold">
           Todo List App
         </h1>
         <AddPost />
       </div>
 
-      <PostList posts={posts} /> */}
-      hello world
+      <PostList posts={posts} />
     </main>
   );
 }
